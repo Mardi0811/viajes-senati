80466326d843c4adc4e87cb88e71e95dc96b725f
Mardi0811 <diegoarcoshuamani71011241@gmail.com>
2024-12-04 11:56:27 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Página cargada correctamente.");
+
+    // Aquí puedes agregar más funcionalidades globales si es necesario.
+});
